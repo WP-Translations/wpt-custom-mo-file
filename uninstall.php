6dadc44ab896b3879f25b77c98fcfa97df38ee24
G3ronim0 <jerome@sadler-jerome.fr>
2016-08-14 00:10:50 +0200

    Bug fix -  uninstall now remove translations files and folder

--- uninstall.php
+++ uninstall.php
@@ -6,5 +6,26 @@ defined( 'WP_UNINSTALL_PLUGIN' ) or die( 'Cheatin&#8217; uh?' );
 // Delete WPTCMF options
 delete_option( 'wptcmf_options' );
 
-// Delete WPTCMF uplaod dir
-unlink( WP_UPLOAD_DIR . '/wpt-custom-mo-file/' );
+/**
+ * Remove all mo files
+ *
+ * @since 1.2.0
+ */
+function __wptcmf_rrmdir( $dir ) {
+
+	if ( ! is_dir( $dir ) ) {
+		@unlink( $dir );
+		return;
+	}
+
+	if ( $globs = glob( $dir . '/*', GLOB_NOSORT ) ) {
+		foreach ( $globs as $file ) {
+			is_dir( $file ) ? __wptcmf_rrmdir( $file ) : @unlink( $file );
+		}
+	}
+
+		@rmdir( $dir );
+
+}
+
+__wptcmf_rrmdir( WP_CONTENT_DIR . '/uploads/wpt-custom-mo-file/' );
