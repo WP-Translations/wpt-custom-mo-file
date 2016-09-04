55a24209b36529ef9afb02f0defbb9fa77524108
G3ronim0 <jerome@sadler-jerome.fr>
2016-09-04 16:29:37 +0200

    Modifiy sprintf structure

--- inc/admin/ui/options.php
+++ inc/admin/ui/options.php
@@ -195,8 +195,7 @@ function _wptcmf_filter_admin_footer_text( $text ) {
 			array( '<a target="_blank" href="http://wordpress.org/support/view/plugin-reviews/wpt-custom-mo-file#postform" >&#9733;&#9733;&#9733;&#9733;&#9733;</a>', '<a target="_blank" href="http://wordpress.org/plugins/wpt-custom-mo-file/" >wordpress.org</a>' ),
 			esc_html_x( 'Add your [stars] on [wp.org] to spread the love.', 'Please do not translate [stars] and [wp.org]', 'wpt-custom-mo-file' )
 		);
-		return
-			sprintf( '%s<a href="http://wp-translations.org/" target="_blank">WP-Translations</a>%s | <a target="_blank" href="http://wordpress.org/support/plugin/wpt-custom-mo-file#postform">%s</a> | %s', esc_html__( 'Visit&nbsp;','wpt-custom-mo-file' ), esc_html__( '&nbsp;Community website', 'wpt-custom-mo-file' ), esc_html__( 'Contact Support', 'wpt-custom-mo-file' ), $change );
+		return sprintf( '%1$s<a href="http://wp-translations.org/" target="_blank">WP-Translations</a>%2$s | <a target="_blank" href="http://wordpress.org/support/plugin/wpt-custom-mo-file#postform">%3$s</a> | %4$s', esc_html__( 'Visit&nbsp;','wpt-custom-mo-file' ), esc_html__( '&nbsp;Community website', 'wpt-custom-mo-file' ), esc_html__( 'Contact Support', 'wpt-custom-mo-file' ), $change );
 	}
 }
 
