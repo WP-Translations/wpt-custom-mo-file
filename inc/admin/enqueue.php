9ae4987f6056224d5944899bbebb9db6422c90b2
G3ronim0 <jerome@sadler-jerome.fr>
2016-09-04 17:43:14 +0200

    Use constante slug for screen detection

--- inc/admin/enqueue.php
+++ inc/admin/enqueue.php
@@ -10,7 +10,7 @@ add_action( 'admin_print_styles', '__wptcmf_load_admin_assets' );
 function __wptcmf_load_admin_assets() {
 	$screen = get_current_screen();
 
-	if ( 'tools_page_wpt-custom-mo-file' === $screen->base ) {
+	if ( 'tools_page_' . WPTCMF_SLUG === $screen->base ) {
 		wp_enqueue_style( 'wptcmf-styles', WPTCMF_URL_CSS . 'wptcmf-styles.css', array(), WPTCMF_VERSION );
 	}
 }
