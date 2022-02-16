a2fe910e235fde8aeb2db0021ae428d0a0418e8d
pedro-mendonca <ped.gaspar@gmail.com>
2022-02-16 16:02:32 +0000

    Move header description below title and remove hardcoded hiphen

--- inc/admin/ui/options.php
+++ inc/admin/ui/options.php
@@ -50,7 +50,10 @@ function wpt_customofile_tools_page() {
 				?>
 
 			</div>
-			<p><strong><?php echo esc_html( WPT_CUSTOMOFILE_PLUGIN_NAME ); ?></strong> - <?php esc_html_e( 'Create, activate, desactivate your own set of rules to get full control of any translations in your WordPress installation.', 'wpt-custom-mo-file' ); ?></p>
+			<p>
+				<strong><?php echo esc_html( WPT_CUSTOMOFILE_PLUGIN_NAME ); ?></strong><br>
+				<?php esc_html_e( 'Create, activate, desactivate your own set of rules to get full control of any translations in your WordPress installation.', 'wpt-custom-mo-file' ); ?>
+			</p>
 		</header>
 		<h2 class="screen-reader-text" style="margin:0;"><?php echo esc_html_x( 'WP-Translations', 'Company name, don\'t translate', 'wpt-custom-mo-file' ); ?></h2>
 		<?php settings_errors(); ?>
