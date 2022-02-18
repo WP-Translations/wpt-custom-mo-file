048af361d3b9181e43d2e0356573ef233827070e
pedro-mendonca <ped.gaspar@gmail.com>
2022-02-18 13:21:45 +0000

    Reorder enqueue

--- inc/admin/enqueue.php
+++ inc/admin/enqueue.php
@@ -51,18 +51,18 @@ function wpt_customofile_load_admin_assets() {
 	);
 
 	wp_register_script(
-		'data-tables-scripts',
-		WPT_CUSTOMOFILE_LIB_URL . 'datatables/jquery.dataTables' . $suffix . '.js',
-		array( 'jquery' ),
-		'1.10.12',
-		true
-	);
-
-	wp_register_script(
 		'wpt-customofile-admin-scripts',
 		WPT_CUSTOMOFILE_JS_URL . 'admin' . $suffix . '.js',
 		array( 'jquery' ),
 		WPT_CUSTOMOFILE_VERSION,
+		true
+	);
+
+	wp_register_script(
+		'data-tables-scripts',
+		WPT_CUSTOMOFILE_LIB_URL . 'datatables/jquery.dataTables' . $suffix . '.js',
+		array( 'jquery' ),
+		'1.10.12',
 		true
 	);
 
