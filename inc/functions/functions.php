b4a84e8be76f10816eae46916179dcabdf4b1ba2
pedro-mendonca <ped.gaspar@gmail.com>
2022-02-19 14:09:45 +0000

    Prepare uploads folder

--- inc/functions/functions.php
+++ inc/functions/functions.php
@@ -26,11 +26,46 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @return array<mixed>   New directory subdir/path/url.
  */
 function wpt_customofile_filter_upload_dir( $upload_dir ) {
-	$upload_dir['subdir'] = '/wpt-custom-mo-file';
-	$upload_dir['path']   = $upload_dir['basedir'] . '/wpt-custom-mo-file';
-	$upload_dir['url']    = $upload_dir['baseurl'] . '/wpt-custom-mo-file';
+
+	$upload_dir['subdir'] = '/' . WPT_CUSTOMOFILE_SLUG;
+	$upload_dir['path']   = $upload_dir['basedir'] . '/' . WPT_CUSTOMOFILE_SLUG;
+	$upload_dir['url']    = $upload_dir['baseurl'] . '/' . WPT_CUSTOMOFILE_SLUG;
+
+	// Prepare uploads folder.
+	wpt_customofile_upload_dir_prepare( $upload_dir );
 
 	return $upload_dir;
+}
+
+
+/**
+ * Prepare uploads folder.
+ * Check if folder exist and has empty index, create both if don't exist.
+ *
+ * @since 1.2.0
+ *
+ * @param array<mixed> $upload_dir   Upload folder data.
+ *
+ * @return void
+ */
+function wpt_customofile_upload_dir_prepare( $upload_dir ) {
+
+	// Checks if the content folder exists.
+	if ( ! is_dir( $upload_dir['path'] ) ) {
+
+		// Create the content folder.
+		mkdir( $upload_dir['path'], 0755, true );
+
+	}
+
+	// Checks if index exists.
+	if ( ! is_file( $upload_dir['path'] . '/index.php' ) ) {
+
+		// Add empty index file.
+		copy( WPT_CUSTOMOFILE_PATH . 'index.php', $upload_dir['path'] . '/index.php' );
+
+	}
+
 }
 
 
